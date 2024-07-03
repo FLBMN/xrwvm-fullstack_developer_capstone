596aacb34b2adc73a964ac2e157bd1ad8de9b888
FLBMN <florian.brueggmann@gmx.de>
2024-07-03 03:13:09 -0400

    Changes

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  const state = req.params.state;
+  try {
+    const documents = await Dealerships.find({ state: state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  const dealerId = req.params.id;
+  try {
+    const document = await Dealerships.findOne({ id: dealerId });
+    if (!document) {
+      res.status(404).json({ error: 'Dealer not found' });
+      return;
+    }
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching document' });
+  }
 });
 
 //Express route to insert review
