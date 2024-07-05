12f1d8a39372a57adab919f4a59cb10299b648b5
FLBMN <florian.brueggmann@gmx.de>
2024-07-05 15:41:52 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
         name = models.CharField(max_length=100)
         description = models.TextField()
-            def __str__(self):
+        def __str__(self):
                 return self.name
 # Car Model model
 class CarModel(models.Model):
