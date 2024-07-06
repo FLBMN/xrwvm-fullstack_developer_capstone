eac4bc71431a518d9c4b4a04b15902d8a8ecdff4
FLBMN <florian.brueggmann@gmx.de>
2024-07-06 07:19:27 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,37 +1,45 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Car Make model
+from datetime import datetime
 
 
 class CarMake(models.Model):
-
-    
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
+    """Model representing a car manufacturer."""
+    name = models.CharField(max_length=100, verbose_name="Car Make Name")
+    description = models.TextField(verbose_name="Description")
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Car Make"
+        verbose_name_plural = "Car Makes"
 
-# Car Model model
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    """Model representing a car model."""
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    name = models.CharField(max_length=100, verbose_name="Car Model Name")
+    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV', verbose_name="Car Type")
+    year = models.IntegerField(
+        default=datetime.now().year,
         validators=[
-            MaxValueValidator(2023),
+            MaxValueValidator(datetime.now().year),
             MinValueValidator(2015)
-        ])
+        ],
+        verbose_name="Model Year"
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.car_make.name} {self.name}"
+
+    class Meta:
+        verbose_name = "Car Model"
+        verbose_name_plural = "Car Models"
